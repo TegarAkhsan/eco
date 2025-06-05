2ebd51aa057b90602f1c5bc77493af3848e2fc2a
TegarAkhsan <tegar.23038@mhs.unesa.ac.id>
2025-06-05 12:15:01 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      // di sini kita set folder output
-      build: {
+    input: ['resources/css/app.css', 'resources/js/app.js'],
+    build: {
         manifest: true,
         outDir: 'public/build',
         emptyOutDir: true,
-      },
+    },
     }),
-    react(),
   ],
 });
