24e86bc18790bced68193e6985501af2fd339e3e
TegarAkhsan <tegar.23038@mhs.unesa.ac.id>
2025-06-05 12:46:30 +0700

    update vite css

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     laravel({
-    input: ['resources/css/app.css', 'resources/js/app.js'],
-    build: {
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      build: {
         manifest: true,
         outDir: 'public/build',
         emptyOutDir: true,
-    },
+      },
     }),
+    react(),
   ],
 });
+
